9e7533330f5d7470188471cae7f383f6c18900be
JulioCesarAlcantara <julio.cal93@gmail.com>
2017-10-20 00:03:16 -0200

    dasdfasd

--- api02.py
+++ api02.py
@@ -1,7 +1,7 @@
 # import os
 from builtins import print
 
-#from pip.utils import encoding
+from pip.utils import encoding
 #encoding: utf-8
 from flask import jsonify, request, Flask, render_template, url_for, session, g
 import json
@@ -627,4 +627,4 @@ def search_all_things_actives(token):
 #     port = int(os.environ.get("PORT", 5000))
 #     app.run(host='0.0.0.0', port=port)
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
